4ede363210307efdc284c07ee2fe05cffd776c9d
DESKTOP-PC\Sebastian Komposch <23136132+SebastiantheAstorian@users.noreply.github.com>
2020-06-29 22:23:14 +0200

    added sample test and config file for pylint

---
+++ sam/test/test_sample.py
@@ -0,0 +1,8 @@
+"""
+This directory contains tests for the SAM application root.
+More specifically, this file contains a placeholder test to make sure the CI/CD pipline does not break
+This is best practice according to: https://github.com/pytest-dev/pytest/issues/2393
+"""
+
+def test_placeholder():
+    pass
