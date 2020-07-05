7cf6d00715cb603dde7e0b849f9eb09ac3e0a7b3
DESKTOP-PC\Sebastian Komposch <23136132+SebastiantheAstorian@users.noreply.github.com>
2020-07-05 22:19:49 +0200

    #14: fix pylint errors, adds mypy config for invalid errors

--- bot/util/utilities.py
+++ bot/util/utilities.py
@@ -1,16 +1,24 @@
+"""Contains a cog for all utility funcionality"""
 from discord.ext import commands
-from datetime import datetime, timedelta
 
 
 class UtilitiesCog(commands.Cog):
+    """Cog for utility functions"""
+
     def __init__(self, bot):
+        """Initializes the Cog"""
         self.bot = bot
 
     @commands.command(name='ping')
     async def ping(self, ctx):
+        """
+        Command Handler for the Ping Command
+        Responds with a message containing 'Pong!'
+        """
         latency = round(self.bot.latency * 1000, 2)
         await ctx.send("\U0001F3D3 **Pong!** - {0} ms".format(latency))
 
 
 def setup(bot):
+    """Activates the cog to the bot."""
     bot.add_cog(UtilitiesCog(bot))
