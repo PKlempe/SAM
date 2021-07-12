3acf0728c6878ca9ca264368fb649bc53b6b9c17
abbcccddddeeeeeffffff <75306520+abbcccddddeeeeeffffff@users.noreply.github.com>
2021-07-12 22:26:39 +0200

    Use a named link to the markdown support page

--- bot/utility/howto_handler.py
+++ bot/utility/howto_handler.py
@@ -20,6 +20,6 @@ def code():
     embed.set_image(url="https://cdn.discordapp.com/attachments/858040409312591873/860573151724175400/howto_code.png")
     embed.add_field(
         name="Weitere Information:",
-        value="Unter https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline- gibt es mehr Infos zu Discords Markdownsyntax"
+        value="Auf der [Discord Support Seite](https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-) gibt es mehr Infos zu Discords Markdownsyntax"
     )
     return embed
